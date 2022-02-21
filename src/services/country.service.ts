8e49eb91c92fe16ff9d3909ea4851b08a1773e44
Mathew Martens <mmartens4@my.wctc.edu>
2022-02-20 21:58:18 -0600

    something

---
+++ src/services/country.service.ts
@@ -0,0 +1,46 @@
+﻿import axios from "axios";
+
+class CountryService {
+    /**
+     * Gets all countries
+     */
+    static async getAllCountries() {
+        return axios.get(`https://mmartens-medals-api-v1.azurewebsites.net/api/country/get-all-countries`,
+        );
+    }
+
+    /**
+     * Returns a specific country based on the id
+     * @param countryId The id of a country to return
+     */
+    static async getCountry(countryId: number){
+        return axios.get(`https://mmartens-medals-api-v1.azurewebsites.net/api/country/get-country/${countryId}`
+        );
+    }
+
+    /**
+     * Adds a country to the application
+     * @param country A country object to add
+     */
+    static async addCountry(country: object) {
+        return axios.post(`https://mmartens-medals-api-v1.azurewebsites.net/api/country/add-country`,
+            { country: {
+                    "id": 1, 
+                    "name": "Rus",
+                    "gold": 4,
+                    "silver": 4,
+                    "bronze": 4
+                } },
+        );
+    }
+
+    /**
+     * Deletes a country from the application
+     * @param countryId The id of a country to remove
+     */
+    static async deleteCountry(countryId: number) {
+        return axios.delete(`https://mmartens-medals-api-v1.azurewebsites.net/api/country/delete/${countryId}`)
+    }
+}
+
+export default CountryService;
